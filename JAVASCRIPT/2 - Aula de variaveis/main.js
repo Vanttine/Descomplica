9aa4ca071809c8cbf7bf5a44c2e4973a8075899d
justrisx <fellipevantine12@gmail.com>
2023-08-13 13:37:21 -0300

    Aula de variaveis

---
+++ JAVASCRIPT/2 - Aula de variaveis/main.js
@@ -0,0 +1,15 @@
+console.log("Testando")
+
+var a = 10;
+var b = 20;
+
+var total = a + b;
+
+console.log(total);
+
+var nome_pessoa = "Lucas";
+
+console.log(nome_pessoa);
+
+
+console.log(nome_pessoa + " Tem " + total + " anos");
